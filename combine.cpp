d5fefcef8ca82ba55a27833ba8589852f1090514
Ben Horley <jz23590@bristol.ac.uk>
2025-03-21 17:35:32 +0000

    Added code to select a random atom of the 100 and calculate the energy change when the spin gets flipped. Also fixed variable name.

--- combine.cpp
+++ combine.cpp
@@ -1,9 +1,19 @@
-#include "Chain.h"
+#include "combine.h"
 #include <iostream>
 #include <cmath> 
 #include <cstdlib> 
 
 
-Chain::Chain(int number_atoms) : interaction_energy(1.0) {
+Combine::Combine(int number_atoms) : interaction_energy(1.0) {
     atoms.resize(number_atoms); 
+}
+
+void Combine::randomise_energy_step(double beta) {
+    
+    int index = rand() % atoms.size();
+    int spin_left = atoms[(index - 1 + atoms.size()) % atoms.size()].current_spin();
+    int spin_right = atoms[(index + 1) % atoms.size()].current_spin();
+    int new_spin = atoms[index].current_spin();
+    double energy_change = 2 * interaction_energy * new_spin * (spin_left + spin_right);
+
 }
