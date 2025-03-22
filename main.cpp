155018b3c171527d44df4f8b5f2f7713080ac9ec
Ben Horley <jz23590@bristol.ac.uk>
2025-03-22 12:14:11 +0000

    I have begun the main.cpp code, bringing together all the other objects. So far I have added the necessary libraries and header files as well as seeding the random number generation at each time and setting the inital variables. Beta is set to 1 for simplicity in the first run.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "montecarlo.h"
+#include "combine.h"      
+#include <cstdlib>       
+#include <time.h>
+#include <iostream>      
+
+int main() {
+    
+    srand(time(0));
+
+    int number_atoms = 100; 
+    double beta = 1.0;      
+    int num_steps = 10000;      
+}
