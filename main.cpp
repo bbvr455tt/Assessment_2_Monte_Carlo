193afc57f97e6495df993bcb67f14bd897573e1c
Ben Horley <jz23590@bristol.ac.uk>
2025-03-24 11:24:13 +0000

    Added the final function which runs the monte carlo simulation with the set number of atoms and set beta value using the functions defined in the monte carlo object to run a simulation for a number of steps and get the energy and magnetization.

--- main.cpp
+++ main.cpp
@@ -11,4 +11,13 @@ int main() {
     int number_atoms = 100; 
     double beta = 1.0;      
     int num_steps = 10000;      
-}
+
+     Montecarlo full_sim(number_atoms, beta);
+
+     full_sim.run_simulation(num_steps);
+ 
+     std::cout << "Energy: " << full_sim.get_current_energy() << std::endl;
+     std::cout << "Magnetization: " << full_sim.get_current_magnetization() << std::endl;
+ 
+     return 0;
+ }
