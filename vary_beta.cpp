7eef550da73a63fcab873ee52b9738299a55900d
Ben Horley <jz23590@bristol.ac.uk>
2025-03-24 17:07:08 +0000

    Set up to loop through beta values and set inital energies and magnetization.

--- vary_beta.cpp
+++ vary_beta.cpp
@@ -8,3 +8,13 @@ varybeta::varybeta(int number_atoms, int num_steps, int num_configurations, cons
       beta_values(beta_values)
       num_steps(num_steps)
       num_configurations(num_configurations) {}
+
+void vary_beta::run(const std::string& output_filename) {
+    std::ofstream data_file_2(vary_beta_1D);
+    data_file << "beta,avg_energy,avg_magnetization\n";
+    
+    for (double beta : beta_values) {
+        double total_energy = 0.0;
+        double total_magnetization = 0.0;
+        }
+}
