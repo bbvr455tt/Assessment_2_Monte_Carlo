ff955efd83c4570aa8e0465f56ad036608e6a3ad
Ben Horley <jz23590@bristol.ac.uk>
2025-03-25 10:56:24 +0000

    Added semicolon, fixed spelling and fixed variable name.

--- vary_beta.cpp
+++ vary_beta.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 #include <cmath>
 
-varybeta::varybeta(int number_atoms, int num_steps, int num_configurations, const std::vector<double>& beta_values)
+vary_beta::vary_beta(int number_atoms, int num_steps, int num_configurations, const std::vector<double>& beta_values)
     : number_atoms(number_atoms),
       beta_values(beta_values)
       num_steps(num_steps)
       num_configurations(num_configurations) {}
 
 void vary_beta::run_all_betas(const std::string& file_name) {
-    std::ofstream data_file_2(vary_beta_1D);
+    std::ofstream data_file_2(vary_beta);
     data_file_2 << "beta,avg_energy,avg_magnetization\n";
     
     for (double beta : beta_values) {
@@ -28,8 +28,8 @@ void vary_beta::run_all_betas(const std::string& file_name) {
         double average_energy = total_energy / num_configurations;
         double average_magnetization = total_magnetization / num_configurations;
         
-        data_file_2 << beta << "," << avergae_energy << "," << average_magnetization << "\n";
+        data_file_2 << beta << "," << average_energy << "," << average_magnetization << "\n";
 
     }
-    data_file_2.close()
+    data_file_2.close();
 }
