7656ad1bd0b875aab53e841c6b0c3f6dcfebae15
Ben Horley <jz23590@bristol.ac.uk>
2025-03-27 15:31:21 +0000

    Added comments to the code.

--- atom.cpp
+++ atom.cpp
@@ -1,16 +1,16 @@
-#include "atom.h"
-#include <cstdlib> 
+#include "atom.h" 
+#include <cstdlib> // Include libraries and header file 
 
 Atom::Atom() {
     int random_num = rand() % 2; 
     spin = (random_num == 1) ? 1 : -1; 
-}
+} // Generates a random spin, either + or -1
 
 void Atom::flip_spin() {
     spin = -spin;
-}
+} // An operation to flip the spin state of an atom.
 
 
 int Atom::current_spin() const {
     return spin;
-}
+} // Allows the spin of the atom to be found and read.
