f34c0f7656d9138c5bd604a0f4140334419ca68a
Ben Horley <jz23590@bristol.ac.uk>
2025-03-27 15:45:41 +0000

    Added comments.

---
+++ vary_beta.h
@@ -0,0 +1,25 @@
+#ifndef VARY_BETA_H 
+#define VARY_BETA_H
+#include "montecarlo.h"
+#include "combine.h"
+#include <string>
+#include <vector>
+
+class vary_beta {
+
+private:
+    int number_atoms;
+    std::vector<double> beta_values;
+    int num_configurations;
+    int num_steps;
+
+public:
+    vary_beta(int number_atoms, int num_steps, int num_configurations, const std::vector<double>& beta_values);
+
+    void run_all_betas(const std::string& filename);
+};
+
+
+
+
+#endif
