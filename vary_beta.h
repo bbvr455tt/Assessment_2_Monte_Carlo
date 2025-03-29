d9b0dee81b27f9959754b28f8a2b1bc41e0263fc
Ben Horley <jz23590@bristol.ac.uk>
2025-03-29 18:03:09 +0000

    Added comments to the code.

--- vary_beta.h
+++ vary_beta.h
@@ -3,7 +3,7 @@
 #include "montecarlo.h"
 #include "combine.h"
 #include <string>
-#include <vector>
+#include <vector> //  Includes necessary inbuilt libraries, header guards and other header files.
 
 class vary_beta {
 
@@ -19,7 +19,9 @@ public:
     void run_all_betas(const std::string& file_name);
 };
 
-
+// Has many private variables, the numbers of steps and configurations and the betas e.t.c are
+// are only used in this class, so can remain provate. THe function of vary_beta and the running of
+// this are public to be implemented in main.cpp.
 
 
 #endif
